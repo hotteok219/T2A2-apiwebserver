3afe486482dbf500766f7c4bd53ebfa367871ba0
hotteok219 <helen-thai@hotmail.com>
2023-07-23 10:56:59 +1000

    Fixing up spacing in cli_controller.py as per PEP8

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -3,17 +3,21 @@ from init import db, bcrypt
 from models.trainers import Trainer
 from models.members import Member
 
+
 db_commands = Blueprint('db', __name__)
+
 
 @db_commands.cli.command('create')
 def create_all():
     db.create_all()
     print('Tables created')
 
+
 @db_commands.cli.command('drop')
 def drop_db():
     db.drop_all()
     print('Tables dropped')
+
 
 @db_commands.cli.command('seed')
 def seed_db():
