578988d9627efcc5e6d8dc9513c6e21c6879b460
hotteok219 <helen-thai@hotmail.com>
2023-07-29 23:24:20 +1000

    Updated validation for trainers and members in controller files

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from sqlalchemy.exc import IntegrityError
+from psycopg2 import errorcodes
 from init import db, bcrypt
 from models.members import Member
 from schemas.member_schema import member_schema, members_schema, memberpw_schema
@@ -63,34 +65,44 @@ def member_delete(id):
 @member_bp.route('/<int:id>', methods=['PUT', 'PATCH'])
 @jwt_required()
 def member_update(id):
-    # Obtain data from user input - using memberpw_schema to enable password updates
-    body_data = memberpw_schema.load(request.get_json(), partial=True)
+    try:
+        # Obtain data from user input - using memberpw_schema to enable password updates
+        body_data = memberpw_schema.load(request.get_json(), partial=True)
 
-    # Look for member with id
-    stmt = db.select(Member).filter_by(id=id)
-    member = db.session.scalar(stmt)
+        # Look for member with id
+        stmt = db.select(Member).filter_by(id=id)
+        member = db.session.scalar(stmt)
 
-    # If member exists, update member
-    if member:
-        # Check if current user is a trainer or is a member with authorisation
-        current_user = get_jwt_identity()
-        if current_user.startswith('member') and str(member.id) != current_user[6:]:
-            return {'error': 'You can only update your own details.'}, 403
+        # If member exists, update member
+        if member:
+            # Check if current user is a trainer or is a member with authorisation
+            current_user = get_jwt_identity()
+            if current_user.startswith('member') and str(member.id) != current_user[6:]:
+                return {'error': 'You can only update your own details.'}, 403
+            else:
+                member.first_name = body_data.get('first_name') or member.first_name
+                member.last_name = body_data.get('last_name') or member.last_name
+                member.dob = body_data.get('dob') or member.dob
+                member.phone = body_data.get('phone') or member.phone
+                member.email =  body_data.get('email') or member.email
+                # Check if there is user input for a password update
+                if body_data.get('password'):
+                    # Check if current user is a member or trainer
+                    if current_user.startswith('member'):
+                        member.password = bcrypt.generate_password_hash(body_data.get('password')).decode('utf-8')
+                    else:
+                        return {'error': 'Only a member can update their own password.'}
+                member.emergency_contact_name =  body_data.get('emergency_contact_name') or member.emergency_contact_name
+                member.emergency_contact_phone = body_data.get('emergency_contact_phone') or member.emergency_contact_phone
+
+                db.session.commit()
+                # Respond to client without showing the password
+                return member_schema.dump(member)
+        # If member doesn't exist, return error
         else:
-            member.first_name = body_data.get('first_name') or member.first_name
-            member.last_name = body_data.get('last_name') or member.last_name
-            member.dob = body_data.get('dob') or member.dob
-            member.phone = body_data.get('phone') or member.phone
-            member.email =  body_data.get('email') or member.email
-            # Check if there is user input for a password update
-            if body_data.get('password'):
-                member.password = bcrypt.generate_password_hash(body_data.get('password')).decode('utf-8')
-            member.emergency_contact_name =  body_data.get('emergency_contact_name') or member.emergency_contact_name
-            member.emergency_contact_phone = body_data.get('emergency_contact_phone') or member.emergency_contact_phone
-
-            db.session.commit()
-            # Respond to client without showing the password
-            return member_schema.dump(member)
-    # If member doesn't exist, return error
-    else:
-        return {'error': f'Member with id {id} does not exist.'}, 404
+            return {'error': f'Member with id {id} does not exist.'}, 404
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            return {'error': 'Email address already in use'}, 409
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            return {'error': f'The {err.orig.diag.column_name} is required'}, 409
--- controllers/trainer_controller.py
+++ controllers/trainer_controller.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from sqlalchemy.exc import IntegrityError
+from psycopg2 import errorcodes
 from init import db, bcrypt
 from models.trainers import Trainer
 from schemas.trainer_schema import trainer_schema, trainers_schema, trainerpw_schema
@@ -61,29 +63,40 @@ def trainer_delete(id):
 @jwt_required()
 @authorise_as_trainer
 def trainer_update(id):
-    # Obtain data from user input - using trainerpw_schema to enable password updates
-    body_data = trainerpw_schema.load(request.get_json(), partial=True)
+    try:
+        # Obtain data from user input - using trainerpw_schema to enable password updates
+        body_data = trainerpw_schema.load(request.get_json(), partial=True)
 
-    # Look for trainer with id
-    stmt = db.select(Trainer).filter_by(id=id)
-    trainer = db.session.scalar(stmt)
+        # Look for trainer with id
+        stmt = db.select(Trainer).filter_by(id=id)
+        trainer = db.session.scalar(stmt)
 
-    # If trainer exists, update trainer
-    if trainer:
-        trainer.first_name = body_data.get('first_name') or trainer.first_name
-        trainer.last_name = body_data.get('last_name') or trainer.last_name
-        trainer.dob = body_data.get('dob') or trainer.dob
-        trainer.phone = body_data.get('phone') or trainer.phone
-        trainer.email =  body_data.get('email') or trainer.email
-        # Check if there is user input for a password update
-        if body_data.get('password'):
-            trainer.password = bcrypt.generate_password_hash(body_data.get('password')).decode('utf-8')
-        trainer.emergency_contact_name =  body_data.get('emergency_contact_name') or trainer.emergency_contact_name
-        trainer.emergency_contact_phone = body_data.get('emergency_contact_phone') or trainer.emergency_contact_phone
-        trainer.first_aid_officer = body_data.get('first_aid_officer') or trainer.first_aid_officer
+        # If trainer exists, update trainer
+        if trainer:
+            trainer.first_name = body_data.get('first_name') or trainer.first_name
+            trainer.last_name = body_data.get('last_name') or trainer.last_name
+            trainer.dob = body_data.get('dob') or trainer.dob
+            trainer.phone = body_data.get('phone') or trainer.phone
+            trainer.email =  body_data.get('email') or trainer.email
+            # Check if there is user input for a password update
+            if body_data.get('password'):
+                    # Check if current user is the trainer being queried
+                    current_user = get_jwt_identity()
+                    if current_user.startswith('trainer') and str(trainer.id) == current_user[7:]:
+                        trainer.password = bcrypt.generate_password_hash(body_data.get('password')).decode('utf-8')
+                    else:
+                        return {'error': 'Only a trainer can update their own password.'}
+            trainer.emergency_contact_name =  body_data.get('emergency_contact_name') or trainer.emergency_contact_name
+            trainer.emergency_contact_phone = body_data.get('emergency_contact_phone') or trainer.emergency_contact_phone
+            trainer.first_aid_officer = body_data.get('first_aid_officer') or trainer.first_aid_officer
 
-        db.session.commit()
-        # Respond to client without showing the password
-        return trainer_schema.dump(trainer)
-    else:
-        return {'error': f'Trainer with id {id} does not exist.'}, 404
+            db.session.commit()
+            # Respond to client without showing the password
+            return trainer_schema.dump(trainer)
+        else:
+            return {'error': f'Trainer with id {id} does not exist.'}, 404
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            return {'error': 'Email address already in use'}, 409
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            return {'error': f'The {err.orig.diag.column_name} is required'}, 409
