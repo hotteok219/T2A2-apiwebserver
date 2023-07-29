632fad574aa21e29cc924a74f975147527a7e2db
hotteok219 <helen-thai@hotmail.com>
2023-07-29 22:23:31 +1000

    Updating comments and errorhandling in main.py

--- main.py
+++ main.py
@@ -11,24 +11,36 @@ from controllers.classlog_controller import classlog_bp
 
 
 def create_app():
-    # create flask app object
+    # Create flask app object
     app = Flask(__name__)
 
+    # Sort schema attributes
     app.json.sort_keys = False
 
+    # Setup environment
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     app.config["JWT_SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
+    # Error handling
     @app.errorhandler(ValidationError)
     def validation_error(err):
         return {'error': err.messages}, 400
+    
+    @app.errorhandler(400)
+    def bad_request(err):
+        return {'error':str(err)}, 400
+    
+    @app.errorhandler(404)
+    def not_found(err):
+        return {'error': str(err)}, 404
 
-    # initialise
+    # Initialise
     db.init_app(app)
     ma.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
 
+    # Register blueprints
     app.register_blueprint(db_commands)
     app.register_blueprint(trainer_bp)
     app.register_blueprint(member_bp)
